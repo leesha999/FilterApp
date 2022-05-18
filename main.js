72c268d0828e44e4fabf71808466d1ad8ad7a974
leesha999 <91823779+leesha999@users.noreply.github.com>
2022-05-18 16:16:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+    dog_nose = loadImage('https://i.postimg.cc/HLVwv6pd/Dog-nose-removebg-preview.png');
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Has Loaded');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+        console.log("Nose x = " + noseX);
+        console.log("Nose y = " + noseY);
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(dog_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot(){
+    save('MyFilterImage.png');
+}
+
+
+
